b9da5772668563c3d0fa447ecbe15dd547d91f66
banteg <4562643+banteg@users.noreply.github.com>
2022-09-04 08:47:17 +0400

    feat: compilers by contract

--- examples/compilers.py
+++ examples/compilers.py
@@ -3,10 +3,15 @@ from collections import Counter
 
 import cbor
 from erigon.kv import ErigonKV, Op
+from erigon.accounts import Account
 from rich import box
 from rich.live import Live
-import json
 from rich.table import Table
+from rich.progress import track, Progress
+from typer import Typer
+from rich.console import Console, Group
+
+app = Typer()
 
 PREFIXES = {
     "6004361015": ("vyper", "0.2.0-0.2.4,0.2.11-0.3.3"),
@@ -53,7 +58,13 @@ def generate_table(data) -> Table:
     table.add_column("version")
     table.add_column("contracts", footer=f"{total}")
     table.add_column("percent")
-    colors = {"solc": "red", "vyper": "green", "proxy": "cyan"}
+    colors = {
+        "solc": "red",
+        "vyper": "green",
+        "proxy": "cyan",
+        "eoa": "yellow",
+        "unknown": "dim",
+    }
 
     for key, value in data.most_common():
         if key is None:
@@ -64,10 +75,12 @@ def generate_table(data) -> Table:
             if isinstance(version, tuple):
                 version = ".".join(map(str, version))
         table.add_row(f"{compiler}", f"{version}", f"{value}", f"{value / total:.3%}")
+
     return table
 
 
-def main():
+@app.command()
+def compilers():
     kv = ErigonKV()
     kv.open("Code")
     compiler_counts = Counter()
@@ -85,5 +98,58 @@ def main():
                 live.update(generate_table(compiler_counts))
 
 
+@app.command()
+def accounts():
+    console = Console()
+    bar = Progress(console=console)
+    code_task = bar.add_task("code")
+    acc_task = bar.add_task("accounts")
+
+    with Live(console=console) as live:
+        compiler_counts = Counter()
+        codehash_to_compiler = {}
+        kv = ErigonKV()
+        kv.open("Code")
+        for i in itertools.count(1):
+            row = kv.read(Op.NEXT)
+            if row.k == b"":
+                break
+            compiler = detect_compiler(row.v)
+            codehash_to_compiler[row.k] = compiler
+            compiler_counts[compiler] += 1
+            if i % 10000 == 0:
+                progress = int.from_bytes(row.k, "big") / 2**256
+                bar.update(code_task, completed=progress * 100)
+                table = generate_table(compiler_counts)
+                live.update(Group(bar, table), refresh=True)
+
+        account_counts: Counter = Counter()
+        kv.open("PlainState", dup_sort=True)
+        key = bytes(20)
+
+        for i in itertools.count(1):
+            row = kv.read(Op.SEEK, k=key)
+            if row.k == b"":
+                live.update(generate_table(account_counts))
+                print("end")
+                break
+
+            if len(row.k) == 20:
+                acc: Account = Account.from_storage(row.k.hex(), row.v)
+                if acc.code_hash:
+                    compiler = codehash_to_compiler.get(acc.code_hash) or ("unknown", "")
+                    account_counts[compiler] += 1
+
+            if i % 10000 == 0:
+                # console.log(row.k.hex())
+                progress = int.from_bytes(row.k[:20], "big") / 2**160
+                bar.update(acc_task, completed=round(progress * 100, 3))
+                table = generate_table(account_counts)
+                live.update(Group(bar, table), refresh=True)
+
+            # increment 20th byte and find the next address
+            key = int.to_bytes(int.from_bytes(row.k[:20], "big") + 1, 20, "big")
+
+
 if __name__ == "__main__":
-    main()
+    app()
