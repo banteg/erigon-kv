9166a5c893a73b1df45a073c6f515cba70579103
banteg <4562643+banteg@users.noreply.github.com>
2022-09-04 23:13:59 +0400

    feat: async api

--- erigon/kv.py
+++ erigon/kv.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from queue import SimpleQueue
 
 import grpc
@@ -13,8 +14,8 @@ class ErigonKV:
         self.reply = self.stub.Tx(iter(self.queue.get, None))
         self.transaction = next(self.reply)
 
-    def open(self, bucket):
-        op = Cursor(op=Op.OPEN, bucketName=bucket)
+    def open(self, bucket, dup_sort=False):
+        op = Cursor(op=Op.OPEN_DUP_SORT if dup_sort else Op.OPEN, bucketName=bucket)
         self.queue.put(op)
         self.cursor = next(self.reply).cursorID
 
@@ -22,3 +23,42 @@ class ErigonKV:
         op = Cursor(op=op, cursor=self.cursor, k=k)
         self.queue.put(op)
         return next(self.reply)
+
+
+class AsyncErigonKV:
+    async def connect(self, private_api="127.0.0.1:9090"):
+        self.channel = grpc.aio.insecure_channel(private_api)
+        self.stub = KVStub(self.channel)
+        self.conn = self.stub.Tx()
+        await self.conn.read()
+
+    def open(self, bucket) -> AsyncErigonCursor:
+        return AsyncErigonCursor(self.conn, bucket)
+
+
+class AsyncErigonCursor:
+    def __init__(self, conn, bucket):
+        self.conn = conn
+        self.bucket = bucket
+
+    async def __aenter__(self):
+        await self.conn.write(Cursor(op=Op.OPEN, bucketName=self.bucket))
+        cursor = await self.conn.read()
+        self.cursor = cursor.cursorID
+        return self
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        await self.conn.write(Cursor(op=Op.NEXT, cursor=self.cursor))
+        return await self.conn.read()
+
+    async def __aexit__(self, exc_t, exc_v, exc_tb):
+        pass
+
+
+async def connect(private_api="127.0.0.1:9090"):
+    kv = AsyncErigonKV()
+    await kv.connect(private_api)
+    return kv
