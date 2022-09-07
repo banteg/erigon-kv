bdc66a3d7db19dfd745b9e735b5ea554266f5885
banteg <4562643+banteg@users.noreply.github.com>
2022-09-07 17:24:16 +0400

    feat: async kv api

--- erigon/kv.py
+++ erigon/kv.py
@@ -1,5 +1,4 @@
-from __future__ import annotations
-from queue import SimpleQueue
+from contextlib import asynccontextmanager
 
 import grpc
 from proto.remote.kv_pb2 import Cursor, Op
@@ -7,60 +6,34 @@ from proto.remote.kv_pb2_grpc import KVStub
 
 
 class ErigonKV:
-    def __init__(self, private_api="127.0.0.1:9090"):
-        self.channel = grpc.insecure_channel(private_api)
-        self.stub = KVStub(self.channel)
-        self.queue = SimpleQueue()
-        self.reply = self.stub.Tx(iter(self.queue.get, None))
-        self.transaction = next(self.reply)
-
-    def open(self, bucket, dup_sort=False):
-        op = Cursor(op=Op.OPEN_DUP_SORT if dup_sort else Op.OPEN, bucketName=bucket)
-        self.queue.put(op)
-        self.cursor = next(self.reply).cursorID
-
-    def read(self, op, k=None):
-        op = Cursor(op=op, cursor=self.cursor, k=k)
-        self.queue.put(op)
-        return next(self.reply)
-
-
-class AsyncErigonKV:
     async def connect(self, private_api="127.0.0.1:9090"):
         self.channel = grpc.aio.insecure_channel(private_api)
         self.stub = KVStub(self.channel)
         self.conn = self.stub.Tx()
         await self.conn.read()
 
-    def open(self, bucket) -> AsyncErigonCursor:
-        return AsyncErigonCursor(self.conn, bucket)
+    @asynccontextmanager
+    async def open(self, bucket):
+        """
+        async with kv.open('bucket') as cursor:
+            await cursor.seek(b'prefix')
 
-    async def scan(self, bucket):
+            async for row in cursor:
+                print(row)
+        """
         await self.conn.write(Cursor(op=Op.OPEN, bucketName=bucket))
         cursor = await self.conn.read()
-
-        while True:
-            await self.conn.write(Cursor(op=Op.NEXT, cursor=cursor.cursorID))
-            row = await self.conn.read()
-            if row.k == b'':
-                break
-            yield row
-            break
-
-        await self.conn.write(Cursor(op=Op.CLOSE, cursor=cursor.cursorID))
-        cursor = await self.conn.read()
+        try:
+            yield ErigonCursor(self.conn, cursor.cursorID)
+        finally:
+            await self.conn.write(Cursor(op=Op.CLOSE, cursor=cursor.cursorID))
+            await self.conn.read()
 
 
-class AsyncErigonCursor:
-    def __init__(self, conn, bucket):
+class ErigonCursor:
+    def __init__(self, conn, cursor):
         self.conn = conn
-        self.bucket = bucket
-
-    async def __aenter__(self):
-        await self.conn.write(Cursor(op=Op.OPEN, bucketName=self.bucket))
-        cursor = await self.conn.read()
-        self.cursor = cursor.cursorID
-        return self
+        self.cursor = cursor
 
     def __aiter__(self):
         return self
@@ -69,11 +42,6 @@ class AsyncErigonCursor:
         await self.conn.write(Cursor(op=Op.NEXT, cursor=self.cursor))
         return await self.conn.read()
 
-    async def __aexit__(self, exc_t, exc_v, exc_tb):
-        pass
-
-
-async def connect(private_api="127.0.0.1:9090"):
-    kv = AsyncErigonKV()
-    await kv.connect(private_api)
-    return kv
+    async def seek(self, key):
+        await self.conn.write(Cursor(op=Op.SEEK, k=key, cursor=self.cursor))
+        return await self.conn.read()
