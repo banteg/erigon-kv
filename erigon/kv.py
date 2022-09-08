32aa76e299d4856f11a25462aeb367ce8b72d70a
banteg <4562643+banteg@users.noreply.github.com>
2022-09-08 19:04:55 +0400

    feat: add dup sort mode

--- erigon/kv.py
+++ erigon/kv.py
@@ -13,7 +13,7 @@ class ErigonKV:
         await self.conn.read()
 
     @asynccontextmanager
-    async def open(self, bucket):
+    async def open(self, bucket, dup_sort=False):
         """
         async with kv.open('bucket') as cursor:
             await cursor.seek(b'prefix')
@@ -21,7 +21,8 @@ class ErigonKV:
             async for row in cursor:
                 print(row)
         """
-        await self.conn.write(Cursor(op=Op.OPEN, bucketName=bucket))
+        op = Op.OPEN_DUP_SORT if dup_sort else Op.OPEN
+        await self.conn.write(Cursor(op=op, bucketName=bucket))
         cursor = await self.conn.read()
         try:
             yield ErigonCursor(self.conn, cursor.cursorID)
@@ -31,15 +32,17 @@ class ErigonKV:
 
 
 class ErigonCursor:
-    def __init__(self, conn, cursor):
+    def __init__(self, conn, cursor, dup_sort=False):
         self.conn = conn
         self.cursor = cursor
+        self.dup_sort = dup_sort
 
     def __aiter__(self):
         return self
 
     async def __anext__(self):
-        return await self.read(Op.NEXT)
+        op = Op.NEXT_DUP if self.dup_sort else Op.NEXT
+        return await self.read(op)
 
     async def read(self, op, key=None, value=None):
         await self.conn.write(Cursor(op=op, k=key, v=value, cursor=self.cursor))
