7caaf8ba23f219f87791339db7216af7e082e0b5
banteg <4562643+banteg@users.noreply.github.com>
2022-09-08 19:21:35 +0400

    feat: update compile proto script

--- compile_proto.py
+++ compile_proto.py
@@ -12,15 +12,16 @@ command.build_package_protos(".")
 dir_names = {dirname(x) for x in glob("**/*.proto")}
 generated = glob("**/*pb2*.py")
 
-search = fr'^from ({"|".join(dir_names)}) import'
-replace = fr"from proto.\1 import"
+search = rf'^from ({"|".join(dir_names)}) import'
+replace = rf"from erigon.proto.\1 import"
 
 for source in generated:
     path = Path(source)
     parts = list(path.absolute().parts)
+    parts = parts[:-3] + ["erigon", "proto"] + parts[-2:]
     parts[-3] = "proto"
     out_path = Path(*parts)
     out_path.parent.mkdir(parents=True, exist_ok=True)
-    lines = path.read_text().split('\n')
-    text = '\n'.join(re.sub(search, replace, line) for line in lines)
+    lines = path.read_text().split("\n")
+    text = "\n".join(re.sub(search, replace, line) for line in lines)
     out_path.write_text(text)
