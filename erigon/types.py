b477f26065312112f5917d2c688b4704d32508f4
banteg <4562643+banteg@users.noreply.github.com>
2022-09-09 03:36:09 +0400

    feat: hash type encoding

--- erigon/types.py
+++ erigon/types.py
@@ -12,9 +12,30 @@ def i(data: bytes):
 
 
 def decode(msg):
-    if isinstance(msg, H128):
-        return b(msg.hi) + b(msg.lo)
-    elif isinstance(msg, H160):
-        return decode(msg.hi) + b(msg.lo, 4)
-    elif isinstance(msg, (H256, H512, H1024, H2048)):
-        return decode(msg.hi) + decode(msg.lo)
+    match msg:
+        case H128():
+            return b(msg.hi) + b(msg.lo)
+        case H160():
+            return decode(msg.hi) + b(msg.lo, 4)
+        case H256() | H512() | H1024() | H2048():
+            return decode(msg.hi) + decode(msg.lo)
+
+    raise TypeError("unsupported type %s", type(msg))
+
+
+def encode(data: bytes):
+    match len(data):
+        case 16:
+            return H128(hi=i(data[:8]), lo=i(data[8:16]))
+        case 20:
+            return H160(hi=encode(data[:16]), lo=i(data[16:20]))
+        case 32:
+            return H256(hi=encode(data[:16]), lo=encode(data[16:32]))
+        case 64:
+            return H512(hi=encode(data[:32]), lo=encode(data[32:]))
+        case 128:
+            return H1024(hi=encode(data[:64]), lo=encode(data[64:128]))
+        case 256:
+            return H2048(hi=encode(data[:128]), lo=encode(data[128:256]))
+
+    raise ValueError("invalid data length %d", len(data))
