590286d3271f93779ccf68e2ee1e9148bcaec912
banteg <4562643+banteg@users.noreply.github.com>
2022-09-09 03:39:44 +0400

    style: cleanup types

--- erigon/types.py
+++ erigon/types.py
@@ -3,20 +3,20 @@
 from erigon.proto.types.types_pb2 import H128, H160, H256, H512, H1024, H2048
 
 
-def b(number: int, size: int = 8):
+def to_bytes(number: int, size: int = 8):
     return number.to_bytes(size, "big")
 
 
-def i(data: bytes):
+def to_int(data: bytes):
     return int.from_bytes(data, "big")
 
 
 def decode(msg):
     match msg:
         case H128():
-            return b(msg.hi) + b(msg.lo)
+            return to_bytes(msg.hi) + to_bytes(msg.lo)
         case H160():
-            return decode(msg.hi) + b(msg.lo, 4)
+            return decode(msg.hi) + to_bytes(msg.lo, 4)
         case H256() | H512() | H1024() | H2048():
             return decode(msg.hi) + decode(msg.lo)
 
@@ -26,16 +26,16 @@ def decode(msg):
 def encode(data: bytes):
     match len(data):
         case 16:
-            return H128(hi=i(data[:8]), lo=i(data[8:16]))
+            return H128(hi=to_int(data[:8]), lo=to_int(data[8:]))
         case 20:
-            return H160(hi=encode(data[:16]), lo=i(data[16:20]))
+            return H160(hi=encode(data[:16]), lo=to_int(data[16:]))
         case 32:
-            return H256(hi=encode(data[:16]), lo=encode(data[16:32]))
+            return H256(hi=encode(data[:16]), lo=encode(data[16:]))
         case 64:
             return H512(hi=encode(data[:32]), lo=encode(data[32:]))
         case 128:
-            return H1024(hi=encode(data[:64]), lo=encode(data[64:128]))
+            return H1024(hi=encode(data[:64]), lo=encode(data[64:]))
         case 256:
-            return H2048(hi=encode(data[:128]), lo=encode(data[128:256]))
+            return H2048(hi=encode(data[:128]), lo=encode(data[128:]))
 
     raise ValueError("invalid data length %d", len(data))
