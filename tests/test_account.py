fcbeed82aef1c4c21eec9e512fdc0f6b41b062ee
banteg <4562643+banteg@users.noreply.github.com>
2022-09-08 19:27:09 +0400

    test: test account decoding

---
+++ tests/test_account.py
@@ -0,0 +1,15 @@
+from erigon.accounts import Account
+
+
+def test_account():
+    # fmt: off
+    data = bytes.fromhex('0f0101074e96eef7886cf6010120aea7d4252f6245f301e540cfbee27d3a88de543af8e49c5c62405d5499fab7e5')
+    code_hash = bytes.fromhex('aea7d4252f6245f301e540cfbee27d3a88de543af8e49c5c62405d5499fab7e5')
+    # fmt: on
+    account = Account.from_storage(data)
+    assert account == Account(
+        nonce=1,
+        balance=22121001282727158,
+        incarnation=1,
+        code_hash=code_hash,
+    )
