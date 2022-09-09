5661ecc9fed5608f794b181bf7df5362239d7dae
banteg <4562643+banteg@users.noreply.github.com>
2022-09-09 17:30:41 +0400

    style: simpler hash encode

--- erigon/types.py
+++ erigon/types.py
@@ -1,6 +1,11 @@
 # erigon uses an accursed encoding for hash types
 # ported from https://github.com/ledgerwatch/erigon-lib/blob/main/gointerfaces/type_utils.go
+from typing import TypeAlias
+
 from erigon.proto.types.types_pb2 import H128, H160, H256, H512, H1024, H2048
+
+HashType: TypeAlias = H128 | H160 | H256 | H512 | H1024 | H2048
+HASH_TYPES = {16: H128, 20: H160, 32: H256, 64: H512, 128: H1024, 256: H2048}
 
 
 def to_bytes(number: int, size: int = 8):
@@ -11,7 +16,7 @@ def to_int(data: bytes):
     return int.from_bytes(data, "big")
 
 
-def decode(msg):
+def decode(msg: HashType) -> bytes:
     match msg:
         case H128():
             return to_bytes(msg.hi) + to_bytes(msg.lo)
@@ -23,19 +28,14 @@ def decode(msg):
     raise TypeError("unsupported type %s", type(msg))
 
 
-def encode(data: bytes):
+def encode(data: bytes) -> HashType:
     match len(data):
         case 16:
             return H128(hi=to_int(data[:8]), lo=to_int(data[8:]))
         case 20:
             return H160(hi=encode(data[:16]), lo=to_int(data[16:20]))
-        case 32:
-            return H256(hi=encode(data[:16]), lo=encode(data[16:32]))
-        case 64:
-            return H512(hi=encode(data[:32]), lo=encode(data[32:]))
-        case 128:
-            return H1024(hi=encode(data[:64]), lo=encode(data[64:]))
-        case 256:
-            return H2048(hi=encode(data[:128]), lo=encode(data[128:]))
+        case 32 | 64 | 128 | 256 as size:
+            half = size // 2
+            return HASH_TYPES[size](hi=encode(data[:half]), lo=encode(data[half:]))
 
     raise ValueError("invalid data length %d", len(data))
